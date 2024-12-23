63342e57f7ad14fd48c294180293fa36f5902219
Betelhem <betelhemyimamd@gmail.com>
2024-12-23 11:34:12 +0300

    add project and client queries

---
+++ server/sampleData.js
@@ -0,0 +1,48 @@
+const projects=[
+    {
+        id:"1",
+        clientId:'1',
+        name:'Project 1',
+        description:'Project 1 description',
+        status:'active',
+
+    },
+    {
+        id:"2",
+        clientId:'1',
+        name:'Project 2',
+        description:'Project 2 description',
+        status:'active',
+
+    },
+    {
+        id:"3",
+        clientId:'1',
+        name:'Project 3',
+        description:'Project 3 description',
+        status:'active',
+    }
+]
+
+const clients=[
+    {
+        id:"1",
+        name:'Client 1',
+        email:'client1@gmail.com',
+        phone:'1234567890',
+    },
+    {
+        id:"2",
+        name:'Client 2',
+        email:'cl2@gmail.com',
+        phone:'1234567890',
+    },
+    {
+        id:"3",
+        name:'Client 3',
+        email:'cl2@gmail.com',
+        phone:'1234567890',
+    }
+]
+
+module.exports = {projects, clients};
